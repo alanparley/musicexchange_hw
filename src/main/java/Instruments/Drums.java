265786b3c0172b310e8a0a6bb087a7a5770a81fc
Alan Parley <alanparley@hotmail.com>
2020-11-28 11:50:53 +0000

    initial drum tests passing aok

--- src/main/java/Instruments/Drums.java
+++ src/main/java/Instruments/Drums.java
@@ -2,15 +2,17 @@ package Instruments;
 
 public class Drums extends Instrument {
 
-    public int numberOfDrums;
+    private int numberOfDrums;
 
     public Drums(String make, String model, String colour, InstrumentType instrumentType, int numberOfDrums) {
         super(make, model, colour, instrumentType);
+        this.numberOfDrums = numberOfDrums;
     }
 
     public int getNumberOfDrums(){
-        return numberOfDrums;
+        return this.numberOfDrums;
     }
+
 
     public String play(){
         return "Doof";
---
+++ src/test/java/DrumsTest.java
@@ -0,0 +1,57 @@
+import Instruments.Drums;
+import Instruments.InstrumentType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DrumsTest {
+
+    Drums drums;
+
+    @Before
+    public void before(){
+        drums = new Drums("Pearl", "DR200", "Silver", InstrumentType.PERCUSSION, 5);
+    }
+
+    @Test
+    public void canGetMake(){
+        assertEquals("Pearl", drums.getMake());
+    }
+
+    @Test
+    public void canGetModel(){
+        assertEquals("DR200", drums.getModel());
+    }
+
+    @Test
+    public void canGetColour(){
+        assertEquals("Silver", drums.getColour());
+    }
+
+    @Test
+    public void canGetInstrumentType(){
+        assertEquals(InstrumentType.PERCUSSION, drums.getInstrumentType());
+    }
+
+   @Test
+    public void canGetNumberOfDrums(){
+        assertEquals(5, drums.getNumberOfDrums());
+   }
+
+   @Test
+    public void canGetBuyingPrice(){
+        assertEquals(0.00, drums.getBuyingPrice(), 0.00);
+   }
+
+   @Test
+    public void canSetBuyingPrice(){
+        drums.setBuyingPrice(800.00);
+        assertEquals(800, drums.getBuyingPrice(), 0.00);
+   }
+
+   @Test
+    public void canPlayInstrument(){
+        assertEquals("Doof", drums.play());
+   }
+}
